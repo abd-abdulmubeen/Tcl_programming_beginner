21e36f11e7c3cc41528fb753210adfa4aaf5f2e2
abd-abdulmubeen <abdul.mubeen10@gmail.com>
2024-12-28 20:52:24 +0530

    Create 2.tcl
    How to save data from the user input using the gets sdin command

---
+++ 2.tcl
@@ -0,0 +1,8 @@
+# to read the user input data from the keyboard and assigning the value to it 
+
+puts "enter the value of a "
+
+set a [gets stdin]
+
+
+puts "the value of a you have entered is a = $a"
